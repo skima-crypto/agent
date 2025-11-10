a971ffdc73d73f15781f8421ac384ee6734bcf8e
Crypto Evangelist <139472454+skima-crypto@users.noreply.github.com>
2025-11-10 12:12:50 +0000

    .

--- 714-chat/src/lib/supabaseServer.ts
+++ 714-chat/src/lib/supabaseServer.ts
@@ -3,6 +3,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const supabaseAdmin = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! 
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 );
-
